daffe684d3e3949c477f89844372a788c835c4a1
Marcus Richter <spirit.is.me@web.de>
2023-01-22 18:46:26 +0100

    added routing

--- boulderball-route-setter/src/app/app-routing.module.ts
+++ boulderball-route-setter/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './views/login/login.component';
+import { RouteSetterComponent } from './views/route-setter/route-setter.component';
+import { StartpageComponent } from './views/startpage/startpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "login",
+    pathMatch: "full"
+  },
+  {
+    path: "login",
+    component: LoginComponent
+  },
+  {
+    path: "start",
+    component: StartpageComponent
+  },
+  {
+    path: "set-route",
+    component: RouteSetterComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- boulderball-route-setter/src/app/app.module.ts
+++ boulderball-route-setter/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -38,7 +37,6 @@ import { StartpageComponent } from './views/startpage/startpage.component';
   ],
   imports: [
     BrowserModule,
-    RouterModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatCardModule,
