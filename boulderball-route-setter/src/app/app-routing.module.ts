15c1821d8781c472fb74a967dfe501237d4943ba
FischerKatharina91 <katharina-fischer1991@web.de>
2023-01-22 20:59:22 +0100

    optimize login and register form

--- boulderball-route-setter/src/app/app-routing.module.ts
+++ boulderball-route-setter/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './views/login/login.component';
+import { RegisterComponent } from './views/register/register.component';
 import { RouteSetterComponent } from './views/route-setter/route-setter.component';
 import { StartpageComponent } from './views/startpage/startpage.component';
 
@@ -13,6 +14,10 @@ const routes: Routes = [
   {
     path: "login",
     component: LoginComponent
+  },
+  {
+    path: "register",
+    component: RegisterComponent
   },
   {
     path: "start",
